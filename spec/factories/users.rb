901fd85ffc3755e5dd946f6cd5b34f72dfd2ce23
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-04 05:42:23 +0900

    ユーザー管理機能

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    
+    nickname {Faker::Name.name}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation { password }
+    last_name { 'あああ' }
+    first_name  { 'いいい' }
+    last_name_kana { person.last.katakana }
+    first_name_kana { person.first.katakana }
+    birthday {0000-00-00}
+  end
+end 
