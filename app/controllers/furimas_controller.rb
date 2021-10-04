c98a7161f6dd975e5d7761615cc92419a20e3a55
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-04 20:57:03 +0900

    ActiveStorageを導入

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -3,5 +3,11 @@ class FurimasController < ApplicationController
   def index
   end
 
-  
+  #念の為記述
+  private
+
+  def message_params
+    params.require(:item_name).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
