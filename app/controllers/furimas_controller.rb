d82f0d925e179e74ce70d3ff2e42e58fa0a5e0d8
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-07 16:08:20 +0900

    出品商品の情報をテーブルに保存

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,4 +1,5 @@
 class FurimasController < ApplicationController
+  #before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.all
@@ -8,13 +9,28 @@ class FurimasController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to root_path(@item)
+    else
+       render :new
+    end
+  
+    
+     #def move_to_index
+      #unless user_signed_in?
+       # redirect_to action: :index
+      #end
+     #end
+  end
+  
 
 
-  #念の為記述
-  #private
+  private
 
-  #def message_params
-    #params.require(:item_name).permit(:content, :image).merge(user_id: current_user.id)
-  #end
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id, :ship_from_address_id, :day_to_ship_id, :price, :user, :image).merge(user_id: current_user.id)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,14 +4,14 @@ class Item < ApplicationRecord
   has_one :puchase
   has_one_attached :image
 
-  belongs_to :item_category_id
-  belongs_to :item_condition_id
-  belongs_to :item_delivery_fee_id
-  belongs_to :ship_from_address_
-  belongs_to :day_to_ship_id
+  #belongs_to :item_category_id
+  #belongs_to :item_condition_id
+  #belongs_to :delivery_fee_id
+  #belongs_to :ship_from_address_id
+  #belongs_to :day_to_ship_id
 
-  validates :item_name, :item_description, :item_category_id, :item_condition_id, :item_delivery_fee_id, :delivery_fee_id, :ship_from_adress_id, :day_to_ship_id, :price, :image, presence: true
+  validates :item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id, :delivery_fee_id, :ship_from_address_id, :day_to_ship_id, :price, :image, presence: true
   validates :price, format: { with: /\A[0-9]+\z/ }
-  validates :price, numericality: { in: 300..9999999 }
-  validates :item_category_id, :item_condition_id, :item_delivery_fee_id, :delivery_fee_id, :ship_from_address_id, :day_to_ship_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :item_category_id, :item_condition_id, :delivery_fee_id, :ship_from_address_id, :day_to_ship_id, numericality: { other_than: 1, message: "can't be blank"}
 end
