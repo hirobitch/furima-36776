fe0ab84da213fb4842039e90db1765a3e62b9188
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-08 20:18:26 +0900

    修正

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,7 +1,7 @@
 class FurimasController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, except: :index
-  
+
   def index
     @items = Item.all
   end
@@ -13,22 +13,20 @@ class FurimasController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-       redirect_to root_path(@item)
+      redirect_to root_path(@item)
     else
-       render :new
+      render :new
     end
   end
-  
+
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id, :ship_from_address_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
-  end
-  
-  def move_to_index
-    unless user_signed_in?
-       redirect_to action: :index
-    end
+    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id,
+                                 :ship_from_address_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
