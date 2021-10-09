e830f9560101b3b4c8e9ef20d0cd3a310d93c099
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-09 14:28:10 +0900

    修正しました

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -3,7 +3,7 @@ class FurimasController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @items = Item.all.order(id: "DESC") 
+    @items = Item.all.order(id: 'DESC')
   end
 
   def new
@@ -26,13 +26,11 @@ class FurimasController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id,:ship_from_address_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id,
+                                 :ship_from_address_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    unless user_signed_in?
-    redirect_to action: :index 
-    end
+    redirect_to action: :index unless user_signed_in?
   end
-  
 end
