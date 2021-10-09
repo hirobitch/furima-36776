837f7ca267ba98372573919fdad26a4c1513cb37
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-09 18:05:19 +0900

    手順6 updateアクション定義

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -25,6 +25,16 @@ class FurimasController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user.id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit 
+    end
   end
 
   private
