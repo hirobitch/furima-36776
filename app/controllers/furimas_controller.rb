36168bc50a20ddeea4b9f38e375416300d35679b
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-09 18:42:10 +0900

    修正

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,6 +1,7 @@
 class FurimasController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -20,20 +21,17 @@ class FurimasController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user.id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to action: :show
     else
-      render :edit 
+      render :edit
     end
   end
 
@@ -46,5 +44,9 @@ class FurimasController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
