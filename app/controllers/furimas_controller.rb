e3e5f1389c33a8df92f98e8071a88b46f5f7f31f
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-09 19:00:27 +0900

    修正

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,6 +1,5 @@
 class FurimasController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -40,10 +39,6 @@ class FurimasController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id,
                                  :ship_from_address_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
   end
 
   def set_item
