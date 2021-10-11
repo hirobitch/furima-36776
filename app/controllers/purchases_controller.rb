2024198b1f1be8f42e4e81d0559a75f941476216
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-11 15:21:29 +0900

    手順3 購入フォームを実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,21 +1,28 @@
 class PurchasesController < ApplicationController
   
   def index
-    #@item = Item.find(params[:id])
+    @item = Item.find(params[:furima_id])
     @purchase_address = PurchaseAddress.new
     #@purchase_address = purchase_address.find(params[:id])
+
   end
 
   def create
-    @purchase = Purchase.create(purchase_params)
-    ShippingAddress.(shipping_address_params)
+    @item = Item.find(params[:furima_id])
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
-  private
+private
 
-  def item_params
-    params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_condition_id, :delivery_fee_id,
-                                 :ship_from_address_id, :day_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  def purchase_params
+  params.require(:purchase_address).permit(:user, :item, :postal_code, :ship_from_address_id, :municipality, :address, :building, :tell, :purchase).merge(user_id: current_user.id)
   end
+
 end
 
