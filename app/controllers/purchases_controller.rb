254311d9ec4b4e0bf4a9b8d4528d9bdf4c48f961
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-14 20:13:04 +0900

    手順3 トークンに対するバリデーションを設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -27,7 +27,7 @@ private
   
   def pay_item
     #binding.pry
-    Payjp.api_key = "sk_≈"  
+    Payjp.api_key = "sk_test_92b889a8f082474cfcc76f7e"  
     Payjp::Charge.create(
       amount: @item.price,  # 商品の値段
       card: purchase_params[:token],    # カードトークン
