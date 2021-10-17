3cb2263d541552f9ff5ccbf974d67b80a0a588eb
hirobitch <89516353+hirobitch@users.noreply.github.com>
2021-10-17 12:58:45 +0900

    修正

--- db/migrate/20211010095731_create_shipping_addresses.rb
+++ db/migrate/20211010095731_create_shipping_addresses.rb
@@ -5,7 +5,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[6.0]
       t.integer :ship_from_address_id, null: false
       t.string :municipality,          null: false
       t.string :address,               null: false
-      t.string :building,
+      t.string :building
       t.string :tell,                  null: false
       t.references :purchase,          null: false, foreign_key: true
       t.timestamps
